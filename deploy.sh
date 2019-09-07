0472e238372190f2c979420181eada3186173535
Nilana Rocha <nilanaarquitetura@gmail.com>
2019-09-07 20:20:54 +1000

    writing command message

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+
+echo "😁 Pushing to heroku"
 git push heroku master 
 
+echo "🤓 Creating database on heroku"
 heroku run rails db:setup
 
+echo "🏃🏽‍♀ Running rails migrations"
 heroku run rails db:migrate
 
+echo "🤩 Running database seed"
 heroku run rails db:seed
+
+echo "🥳 All good. Enjoy!"
