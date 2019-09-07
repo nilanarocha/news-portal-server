2008d8c2d7ed5fb30bfb382d132925fde34ae414
Nilana Rocha <nilanaarquitetura@gmail.com>
2019-09-07 20:05:55 +1000

    fixing home url by redirecting to news

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
   get '/api/news-category' => 'api_news#get_news_category'
   get '/api/search' => 'api_news#get_search'
 
+  get '/', to: redirect('/news')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
